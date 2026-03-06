fc1b5c7e3ff58394f4f7e6957fc3d669ecc12e29
BaldurLogi <baldurlogi16@icloud.com>
2026-03-06 23:31:11 +0100

    minor fix

--- src/features/goals/UserGoalPage.tsx
+++ src/features/goals/UserGoalPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
